5ceb67b1f396271fc35b1d4bc37098f670909a35
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-09-24 23:40:45 +0400

    refactor: Prettier + removed commented JS

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Select page elements and assign them to variables
 let button = document.querySelector("button");
 let enterButton = document.getElementById("enter");
@@ -11,54 +10,53 @@ let deleteButton = document.getElementsByClassName("delete-button");
 h2List = document.querySelectorAll("h2");
 
 for (let i = 0; i < h2List.length; i++) {
-    h2List[i].className = "box-title";
+	h2List[i].className = "box-title";
 }
 
 // Functions for creating list element with delete button
 function inputLength(input) {
-    return input.value.length;
+	return input.value.length;
 }
 
 function createListElement(list, input) {
-    let li = document.createElement("li");
-    let span = document.createElement("span");
+	let li = document.createElement("li");
+	let span = document.createElement("span");
 
-    span.appendChild(document.createTextNode(input.value));
-    li.appendChild(span);
-    list.insertBefore(li, list.childNodes[0]);
-    li.classList.add("shopping-item");
-    input.value = "";
-    span.addEventListener('click', listItemComplete, false);
+	span.appendChild(document.createTextNode(input.value));
+	li.appendChild(span);
+	list.insertBefore(li, list.childNodes[0]);
+	li.classList.add("shopping-item");
+	input.value = "";
+	span.addEventListener("click", listItemComplete, false);
 
-    let button = document.createElement("button");
-    button.appendChild(document.createTextNode("Delete"));
-    li.appendChild(button);
-    button.classList.add("delete-button");
-    button.addEventListener("click", deleteListItem, false);
+	let button = document.createElement("button");
+	button.appendChild(document.createTextNode("Delete"));
+	li.appendChild(button);
+	button.classList.add("delete-button");
+	button.addEventListener("click", deleteListItem, false);
 }
 
-
 function addListAfterClick() {
-    if (inputLength(shoppingListInput) > 0) {
-        createListElement(shoppingList, shoppingListInput);
-    }
+	if (inputLength(shoppingListInput) > 0) {
+		createListElement(shoppingList, shoppingListInput);
+	}
 }
 
 function addListAfterKeyPress(event) {
-    if (inputLength(shoppingListInput) > 0 && event.keyCode === 13) {
-        createListElement(shoppingList, shoppingListInput);
-    }
+	if (inputLength(shoppingListInput) > 0 && event.keyCode === 13) {
+		createListElement(shoppingList, shoppingListInput);
+	}
 }
 
 // Function for marking item complete
 function listItemComplete() {
-    this.classList.toggle("done");
-};
+	this.classList.toggle("done");
+}
 
 // Function for deleting list item
 function deleteListItem() {
-    this.parentNode.remove();
-};
+	this.parentNode.remove();
+}
 
 // Event listener
 enterButton.addEventListener("click", addListAfterClick);
@@ -66,31 +64,9 @@ enterButton.addEventListener("click", addListAfterClick);
 shoppingListInput.addEventListener("keypress", addListAfterKeyPress);
 
 for (var i = 0; i < shoppingListItem.length; i++) {
-    shoppingListItem[i].addEventListener('click', listItemComplete, false);
-};
-
+	shoppingListItem[i].addEventListener("click", listItemComplete, false);
+}
 
 for (var i = 0; i < deleteButton.length; i++) {
-    deleteButton[i].addEventListener("click", deleteListItem, false)
-    };
-
-
-
-
-
-
-
-
-    
-    // button.addEventListener("click", function() {
-//     document.getElementById("shopping-list")
-// })
-
-// button.addEventListener("mouseenter", function() {
-//     console.log("MOUSE!!!");
-// })
-
-// button.addEventListener("mouseleave", function() {
-//     console.log("NO MOUSE!!!");
-// })
-
+	deleteButton[i].addEventListener("click", deleteListItem, false);
+}
