753780d0b76249e7cd409d2fd67b4629bb61b24e
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-09-25 11:47:28 +0400

    refactor: Abstract out addDeleteButton, convert to arrow functions

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Select page elements and assign them to variables
 let button = document.querySelector("button");
-let enterButton = document.getElementById("enter");
-let shoppingListInput = document.getElementById("user-input");
 let shoppingList = document.getElementById("shopping-list");
 let shoppingListItem = document.querySelectorAll(".shopping-item span");
 let deleteButton = document.getElementsByClassName("delete-button");
+let enterButton = document.getElementById("enter");
+let shoppingListInput = document.getElementById("user-input");
 
 // Apply H2 styles dynamically (just for practice)
 h2List = document.querySelectorAll("h2");
@@ -14,36 +14,54 @@ for (let i = 0; i < h2List.length; i++) {
 }
 
 // Functions for creating list element with delete button
-function inputLength(input) {
+const getInputLength = (input) => {
 	return input.value.length;
 }
 
-function createListElement(list, input) {
+
+const addDeleteButton = (parentElement) => {
+	/* Create new button element */
+	let button = document.createElement("button");
+	
+	/* Add text/class to button */
+	button.appendChild(document.createTextNode("Delete"));
+	button.classList.add("delete-button");
+
+	/* Add button to parent element */
+	parentElement.appendChild(button);
+
+	/* Add event listener that calls delete function when button is clicked */
+	button.addEventListener("click", deleteListItem, false);
+}
+
+
+
+const createListElement = (list, input) => {
+	/* Create new li/span element */
 	let li = document.createElement("li");
 	let span = document.createElement("span");
 
+	/* Grab input value from event listener, insert into span */
 	span.appendChild(document.createTextNode(input.value));
+	/* Append span to li */
 	li.appendChild(span);
+
 	list.insertBefore(li, list.childNodes[0]);
 	li.classList.add("shopping-item");
 	input.value = "";
 	span.addEventListener("click", listItemComplete, false);
 
-	let button = document.createElement("button");
-	button.appendChild(document.createTextNode("Delete"));
-	li.appendChild(button);
-	button.classList.add("delete-button");
-	button.addEventListener("click", deleteListItem, false);
+	addDeleteButton(li);
 }
 
 function addListAfterClick() {
-	if (inputLength(shoppingListInput) > 0) {
+	if (getInputLength(shoppingListInput) > 0) {
 		createListElement(shoppingList, shoppingListInput);
 	}
 }
 
 function addListAfterKeyPress(event) {
-	if (inputLength(shoppingListInput) > 0 && event.keyCode === 13) {
+	if (getInputLength(shoppingListInput) > 0 && event.keyCode === 13) {
 		createListElement(shoppingList, shoppingListInput);
 	}
 }
